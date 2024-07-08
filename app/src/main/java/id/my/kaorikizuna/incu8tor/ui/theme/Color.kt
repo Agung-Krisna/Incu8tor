7141ddbc673c3e0824dcb90f9154c935ad348130
kaorikizuna <34364395+Agung-Krisna@users.noreply.github.com>
2024-07-08 17:01:39 +0800

    feat: added new colors

--- app/src/main/java/id/my/kaorikizuna/incu8tor/ui/theme/Color.kt
+++ app/src/main/java/id/my/kaorikizuna/incu8tor/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val DarkRed = Color(0x66B71C1C)
+val Red = Color(0xFFEF5350)
+
+val DarkBlue = Color(0xFF1A237E)
+val Blue = Color(0xFF42A5F5)
