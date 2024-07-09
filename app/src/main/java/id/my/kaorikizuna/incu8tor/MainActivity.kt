bdec71ba7807e9846501b9052f4ecb34654628a7
priskapio <elisabethpriskapio04@gmaol.com>
2024-07-09 17:32:54 +0800

    feat: added device detail

--- app/src/main/java/id/my/kaorikizuna/incu8tor/MainActivity.kt
+++ app/src/main/java/id/my/kaorikizuna/incu8tor/MainActivity.kt
@@ -1,18 +1,11 @@
 package id.my.kaorikizuna.incu8tor
 
-import android.bluetooth.BluetoothClass.Device
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.ui.platform.LocalContext
-import id.my.kaorikizuna.incu8tor.ui.deviceConfiguration.DeviceConfigurationScreen
-import id.my.kaorikizuna.incu8tor.ui.deviceConfiguration.DeviceConfigurationScreenPreview
-import id.my.kaorikizuna.incu8tor.ui.home.HomeScreen
+import id.my.kaorikizuna.incu8tor.ui.deviceDetail.DeviceDetailScreenPreview
 import id.my.kaorikizuna.incu8tor.ui.theme.Incu8torTheme
-import id.my.kaorikizuna.incu8tor.viewmodel.DeviceViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +17,7 @@ class MainActivity : ComponentActivity() {
 //                val deviceDetail = viewModel.getDevice("24:DC:C3:45:EA:CC")
 //                Log.d(TAG, "device: ${deviceDetail.toString()}")
 //                DeviceConfigurationScreen(deviceDetail)
-                DeviceConfigurationScreenPreview()
+                DeviceDetailScreenPreview()
             }
         }
     }
--- app/src/main/java/id/my/kaorikizuna/incu8tor/ui/deviceDetail/DeviceDetailScreen.kt
+++ app/src/main/java/id/my/kaorikizuna/incu8tor/ui/deviceDetail/DeviceDetailScreen.kt
@@ -1,32 +1,54 @@
-package id.my.kaorikizuna.incu8tor.ui.detail
+package id.my.kaorikizuna.incu8tor.ui.deviceDetail
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.material3.icons.Icons
-import androidx.compose.material3.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import id.my.kaorikizuna.incu8tor.R
 import id.my.kaorikizuna.incu8tor.model.DeviceDetail
 import id.my.kaorikizuna.incu8tor.ui.theme.Incu8torTheme
+import id.my.kaorikizuna.incu8tor.viewmodel.DeviceViewModel
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.ui.graphics.ColorFilter
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+import java.util.Date
+import java.util.Locale
+import androidx.compose.material3.OutlinedCard as OutlinedCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DeviceDetailScreen(deviceDetail: DeviceDetail, onBackClicked: () -> Unit) {
+fun DeviceDetailScreen(deviceDetail: DeviceDetail, onBackClicked: () -> Unit, onSettingsClicked: () -> Unit) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "Incu8tor") },
+                title = { Text(text = deviceDetail.deviceName, style = MaterialTheme.typography.titleSmall) },
                 navigationIcon = {
                     IconButton(onClick = { onBackClicked() }) {
                         Icon(
                             imageVector = Icons.Filled.ArrowBack,
                             contentDescription = "Back"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { onSettingsClicked() }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_setting),
+                            contentDescription = "Settings"
                         )
                     }
                 }
@@ -38,38 +60,105 @@ fun DeviceDetailScreen(deviceDetail: DeviceDetail, onBackClicked: () -> Unit) {
                     .padding(innerPadding)
                     .padding(16.dp)
                     .fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Image(
-                        painter = painterResource(id = R.mipmap.ic_temperature),
-                        contentDescription = "Temperature"
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        text = "Temperature: ${deviceDetail.sensors.temperature} °C",
-                        style = MaterialTheme.typography.bodyMedium
-                    )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    OutlinedCard(
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.size(100.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surface
+                        )
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_temperature),
+                                contentDescription = "Temperature",
+                                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "${deviceDetail.sensors.temperature} °C",
+                                style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold)
+                            )
+                        }
+                    }
+                    OutlinedCard(
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.size(100.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surface
+                        )
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_humidity),
+                                contentDescription = "Humidity",
+                                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "${deviceDetail.sensors.humidity} %",
+                                style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold)
+                            )
+                        }
+                    }
                 }
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Image(
-                        painter = painterResource(id = R.mipmap.ic_humidity),
-                        contentDescription = "Humidity"
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(
-                        text = "Humidity: ${deviceDetail.sensors.humidity} %",
-                        style = MaterialTheme.typography.bodyMedium
-                    )
+                Spacer(modifier = Modifier.height(16.dp))
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(5.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("Start Date", style = MaterialTheme.typography.titleSmall)
+                    OutlinedCard(modifier = Modifier.fillMaxWidth()) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(getDateFromEpoch(deviceDetail.dayStart))
+                        }
+                    }
                 }
-                Text(
-                    text = "Start Date: ${deviceDetail.dayStart}",  // Format sesuai dengan kebutuhan
-                    style = MaterialTheme.typography.bodyMedium
-                )
-                Text(
-                    text = "Egg Date: ${deviceDetail.settings.temperature.max}",  // Contoh, ganti dengan field yang sesuai dari DeviceDetail
-                    style = MaterialTheme.typography.bodyMedium
-                )
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(5.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("Egg Date", style = MaterialTheme.typography.titleSmall)
+                    OutlinedCard(modifier = Modifier.fillMaxWidth()) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            val daysBetween = calculateDaysBetween(deviceDetail.dayStart)
+                            Text("$daysBetween days")
+                        }
+                    }
+                }
+
                 Spacer(modifier = Modifier.weight(1f))
                 Button(
                     onClick = { /* TODO: Implement action */ },
@@ -82,13 +171,31 @@ fun DeviceDetailScreen(deviceDetail: DeviceDetail, onBackClicked: () -> Unit) {
     )
 }
 
+fun calculateDaysBetween(startDateEpoch: Long): Long {
+    val currentDate = LocalDate.now()
+    val startDate = LocalDate.ofEpochDay(startDateEpoch / (24 * 60 * 60))
+    return ChronoUnit.DAYS.between(startDate, currentDate)
+}
+
+fun getDateFromEpoch(epochTime: Long): String {
+    return SimpleDateFormat("dd MMMM yyyy, HH:mm", Locale.getDefault()).format(Date(epochTime * 1000))
+}
+
 @Preview(showBackground = true)
 @Composable
-fun PreviewDeviceDetailScreen() {
+fun DeviceDetailScreenPreview() {
+    val viewModel = DeviceViewModel()
+    val (deviceDetail, setDeviceDetail) = remember { mutableStateOf(DeviceDetail()) }
+    LaunchedEffect(Unit) {
+        viewModel.getDevice("24:DC:C3:45:EA:CC", onSuccess = {
+            setDeviceDetail(it)
+        })
+    }
     Incu8torTheme {
         DeviceDetailScreen(
-            deviceDetail = DeviceDetail(),  // Ganti dengan instance DeviceDetail yang sebenarnya
-            onBackClicked = { /* Preview action */ }
+            deviceDetail = deviceDetail,
+            onBackClicked = { /* Preview action */ },
+            onSettingsClicked = { /* Preview action */ }
         )
     }
 }
